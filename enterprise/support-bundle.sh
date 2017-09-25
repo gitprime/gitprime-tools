17a3ae2cbb80580705b02585b64c2a22ff03add7
Todd Tidwell <toddjtidwell@gmail.com>
2017-09-24 21:00:55 -0600

    Added new logic for gitprime logs

--- enterprise/support-bundle.sh
+++ enterprise/support-bundle.sh
@@ -24,6 +24,8 @@ mkdir -p "${GITPRIME_LOG_DIR}"
 # Get the list of as many containers as we can, including ones that have died.
 REPLICATED_CONTAINER_LIST=$(docker ps -a --format="{{.Names}}")
 
+echo "Extracting docker logs for all containers"
+
 # Use those container names to get the docker logs.  This will give us the
 # any logging from the entry points.
 while read -r CONTAINER_NAME; do
@@ -31,19 +33,39 @@ while read -r CONTAINER_NAME; do
 done <<< "${REPLICATED_CONTAINER_LIST}"
 
 # Get the list of currently running containers.  We're going to use these to try and grab
-# the GitPrime Enterprise application logs.
-GITPRIME_CONTAINER_LIST=$(docker ps --format="{{.NAMES}}")
+# the GitPrime Enterprise application logs.  We can only do that for the web, scheduler, and worker
+# containers
+GITPRIME_CONTAINER_LIST=$(docker ps --format="{{.Names}}" | grep -P "replicated_[0-9a-f]{32}_(web|scheduler|worker)\..*")
+
+echo "Extracting GitPrime Enterprise logs"
 
 # Loop through and get anything in /var/log/gitprime
-while read -r CONTAINER_NAME; do
- docker logs ${CONTAINER_NAME} >> "${DOCKER_LOG_DIR}/${CONTAINER_NAME}.log" 2>&1
-done <<< "${REPLICATED_CONTAINER_LIST}"
+while read -r CONTAINER_NAME;
+do
+ mkdir -p "${GITPRIME_LOG_DIR}/${CONTAINER_NAME}"
+
+ # We need a list of log files
+ LOG_FILE_NAMES=$(docker exec "${CONTAINER_NAME}" ls /var/log/gitprime/)
+
+ while read -r LOG_FILE_NAME;
+ do
+    docker cp "${CONTAINER_NAME}:/var/log/gitprime/${LOG_FILE_NAME}" "${GITPRIME_LOG_DIR}/${CONTAINER_NAME}/${LOG_FILE_NAME}"
+ done <<< "${LOG_FILE_NAMES}"
+
+done <<< "${GITPRIME_CONTAINER_LIST}"
+
+echo "Creating support bundle..."
+
+SUPPORT_BUNDLE_NAME="replicated-logs-${TIMESTAMP}.tar.gz"
 
 # Tar/GZ it all up
-tar -C "${TMP_LOG_DIR_ROOT}" -czf "replicated-logs-${TIMESTAMP}.tar.gz" ${TMP_LOG_DIR_NAME}
+tar -C "${TMP_LOG_DIR_ROOT}" -czf "${SUPPORT_BUNDLE_NAME}" ${TMP_LOG_DIR_NAME}
+
+echo "Created support bundle: ${SUPPORT_BUNDLE_NAME}"
 
 # Clean up after ourselves because we're not slobs
 rm -fr ${TMP_LOG_DIR}
 
 # Put IFS back, just because we should
 IFS=$OLD_IFS
+
